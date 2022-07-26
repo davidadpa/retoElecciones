cb133ef4a48c7e72ea92d12eb0c8ef54f3d2e8ca
David <David@LAPTOP-UFCK75MQ>
2022-07-25 23:59:12 -0500

    Carga Inicial

---
+++ proyecto_eleccion/src/main/java/Clases/ClsPersona.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author David
+ */
+public class ClsPersona {
+    
+    private String numeroCedula;
+    private String nombre;
+    private String telefono;
+    private String ciudadOrigen;
+    private String direccion;
+
+    public ClsPersona(String numeroCedula, String nombre, String telefono, String ciudadOrigen, String direccion) {
+        this.numeroCedula = numeroCedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.ciudadOrigen = ciudadOrigen;
+        this.direccion = direccion;
+    }
+    
+  
+    }
+   
+}
